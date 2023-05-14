ec765951ad1168dd7830bca7e467a7b338982c39
Jon Recarte <jonreka99@gmail.com>
2023-05-14 20:13:10 +0200

    Implementar 2. capacidad nativa #27

--- componentes/ContactoComponent.js
+++ componentes/ContactoComponent.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, Button } from 'react-native';
 import { Card } from '@rneui/themed';
+import * as MailComposer from 'expo-mail-composer';
 
 function RenderContacto() {
 
-    return(
+    // Función para manejar el botón de correo electrónico
+    const enviarCorreo = () => {
+        MailComposer.composeAsync({
+            recipients: ['gaztaroa@gaztaroa.com'],
+            subject: 'Información de contacto',
+            body: 'Hola,'
+        });
+    }
+
+    return (
         <Card>
-        <Card.Title>Información de contacto</Card.Title>
-        <Card.Divider />
-        <Text style={{ margin: 20 }}>
-            Kaixo Mendizale! {"\n"}{"\n"}
-            Si quieres participar en las salidas de montaña que organizamos o quieres hacerte soci@ de Gaztaroa, puedes contactar con nosotros a través de diferentes medios. Puedes llamarnos por teléfono los jueves de las semanas que hay salida (de 20:00 a 21:00). También puedes ponerte en contacto con nosotros escribiendo un correo electrónico, o utilizando la aplicación de esta página web. Y además puedes seguirnos en Facebook.{"\n"}{"\n"}
-            Para lo que quieras, estamos a tu disposición!{"\n"}{"\n"}
-            Tel: +34 948 277151{"\n"}{"\n"}
-            Email: gaztaroa@gaztaroa.com
-        </Text>
-    </Card >
+            <Card.Title>Información de contacto</Card.Title>
+            <Card.Divider />
+            <Text style={{ margin: 20 }}>
+                Kaixo Mendizale! {"\n"}{"\n"}
+                Si quieres participar en las salidas de montaña que organizamos o quieres hacerte soci@ de Gaztaroa, puedes contactar con nosotros a través de diferentes medios. Puedes llamarnos por teléfono los jueves de las semanas que hay salida (de 20:00 a 21:00). También puedes ponerte en contacto con nosotros escribiendo un correo electrónico, o utilizando la aplicación de esta página web. Y además puedes seguirnos en Facebook.{"\n"}{"\n"}
+                Para lo que quieras, estamos a tu disposición!{"\n"}{"\n"}
+                Tel: +34 948 277151{"\n"}{"\n"}
+                Email: gaztaroa@gaztaroa.com
+            </Text>
+            <Button
+                title="Enviar correo electrónico"
+                onPress={() => enviarCorreo()}
+            />
+        </Card>
     );
 
 }
@@ -23,7 +37,7 @@ function RenderContacto() {
 class Contacto extends Component {
 
     render() {
-        return(<RenderContacto/>);
+        return (<RenderContacto />);
     }
 }
 
--- componentes/EventComponent.js
+++ componentes/EventComponent.js
@@ -1,35 +1,54 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Alert } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { Platform, Linking } from 'react-native';
+import * as ExpoCalendar from 'expo-calendar';
 
 const EventComponent = () => {
     const [selectedDate, setSelectedDate] = useState(null);
     const [showDateTimePicker, setShowDateTimePicker] = useState(false);
-    const [alarmTime, setAlarmTime] = useState(null);
     const [selectedTime, setSelectedTime] = useState(new Date());
 
     const handleConfirm = (date) => {
-        setSelectedDate(date);
-        setShowDateTimePicker(true);
-        console.log('selectedDate:', selectedDate);
-    };
-
-    const handleAlarm = () => {
-        if (alarmTime) {
-            Linking.openURL(`alarm://?alarmtime=${alarmTime}`);
+        const selectedDate = new Date(date.timestamp);
+        if (selectedDate.setHours(0, 0, 0, 0) >= new Date().setHours(0, 0, 0, 0)) {
+            setSelectedDate(date);
+            setShowDateTimePicker(true);
         }
     };
 
-    const handleDateTimeChange = (event, date) => {
-        if (date) {
-            setSelectedTime(date);
-            const newDate = new Date(selectedDate.year, selectedDate.month - 1, selectedDate.day, date.getHours(), date.getMinutes());
-            setAlarmTime(newDate.getTime());
-            console.log('selectedTime:', selectedTime);
-        }
+    const handleDateTimeChange = async (event, date) => {
         setShowDateTimePicker(false);
+
+        if (date && selectedDate) {
+            const selectedDateTime = new Date(selectedDate.timestamp);
+            selectedDateTime.setHours(date.getHours(), date.getMinutes());
+
+            if (selectedDateTime < new Date()) {
+                setSelectedTime(new Date());
+            } else {
+                setSelectedTime(date);
+            }
+
+            // Crear evento en el calendario
+            const calendarId = await getCalendarId();
+            const eventDetails = {
+                title: 'Nuevo evento',
+                startDate: selectedDateTime,
+                endDate: selectedDateTime,
+                timeZone: 'America/New_York',
+            };
+            const eventId = await ExpoCalendar.createEventAsync(calendarId, eventDetails);
+            Alert.alert('Evento creado', `El evento con ID ${eventId} ha sido creado en el calendario`);
+        }
+    };
+
+    const minimumDate = new Date(); // Fecha actual
+
+    // Función para obtener el ID del calendario
+    const getCalendarId = async () => {
+        const calendar = await ExpoCalendar.getDefaultCalendarAsync();
+        return calendar.id;
     };
 
     return (
@@ -40,20 +59,18 @@ const EventComponent = () => {
                     mode="time"
                     value={selectedTime}
                     is24Hour={true}
-                    display="default"
+                    minimumDate={minimumDate}
                     onChange={handleDateTimeChange}
                 />
             )}
             {selectedDate && (
                 <Text>
-                    Fecha seleccionada: {selectedDate.day}/{selectedDate.month}/{selectedDate.year}
+                    Fecha seleccionada: {selectedDate.day}/{selectedDate.month}/{selectedDate.year}{'   '}
+                    {selectedTime.getHours()}:{selectedTime.getMinutes()}
                 </Text>
             )}
-            <Button title="Establecer alarma" onPress={handleAlarm} disabled={!alarmTime} />
         </View>
     );
 };
 
 export default EventComponent;
-
-
